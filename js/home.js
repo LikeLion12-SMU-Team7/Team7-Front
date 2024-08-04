15e57f4867b42ce86d0556577e4720e0d7c4edee
yoojin <everywavyday@gmail.com>
2024-08-04 21:17:57 +0900

    ✨ feat: 홈 대시보드 API 연동 및 기능 구현 완료 (광고 포함)

--- js/home.js
+++ js/home.js
@@ -303,4 +303,22 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
 
+    let currentIndex = 0;
+    const slides = document.querySelector('.slides');
+    const dots = document.querySelectorAll('.dot');
+    const totalSlides = document.querySelectorAll('.slide').length;
+
+    const updateDots = () => {
+        dots.forEach((dot, index) => {
+            dot.classList.toggle('active', index === currentIndex);
+        });
+    };
+
+    setInterval(() => {
+        currentIndex = (currentIndex + 1) % totalSlides;
+        const offset = -currentIndex * 100;
+        slides.style.transform = `translateX(${offset}%)`;
+        updateDots();
+    }, 2500); // 슬라이드 변경 간격을 3초로 설정
 });
+
